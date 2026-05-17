b2b9fb23695ff8739f152359ba950b5c2d85a460
YamenAly <yamen.halim@gmail.com>
2026-05-17 19:11:00 +0300

    Patients models added

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    date_of_birth = models.DateField(null=True)
+    gender = models.BooleanField(null=True)
+    phone = models.CharField(max_length=255)
+    address = models.CharField()
+    medical_history_notes = models.CharField()
