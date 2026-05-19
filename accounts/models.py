7ff9cf13e1af84eb02ece8039e1cc8250e830378
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2026-05-19 22:51:50 +0300

    Abstraced User, User Admin
    Added custom User modle from AbstractUser
    Registered User model to Admin Panal with all it's field

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+    role = models.CharField(max_length=10, null=False)
+    is_approved = models.BooleanField(default=True)
+    is_blocked = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
