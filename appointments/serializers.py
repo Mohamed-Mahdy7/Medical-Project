d5ada02b2cfb5c49829eeaeae7e8fb5489567ace
Fiow00 <ahmedyhya5325500@gmail.com>
2026-05-21 20:11:12 +0300

    Added permissions

--- appointments/serializers.py
+++ appointments/serializers.py
@@ -4,48 +4,19 @@ from rest_framework import serializers
 
 from .models import Appointment
 
-class AppointmentSerializer(serializers.ModelSerializer):
+class AppointmentCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
-        fields = "__all__"
-
-    read_only_fields = (
-        "patient",
-        "status",
-        "create_at",
-        "updated_at",
-    )
-
-    def update(self, instance, validated_data):
-        validated_data.pop("patient", None)
-        validated_data.pop("doctor", None)
-
-        return super().update(instance, validated_data)
-
-    def validate_appoientment_date(self, value):
-        if value < timezone.now():
-            raise serializers.ValidationError("Appointment cannot be in the past.")
-
-        return value
+        fields = ["doctor", "start_time", "end_time"]
 
     def validate(self, attrs):
         doctor = attrs.get("doctor")
+        start_time = attrs.get("start_time")
+        end_time = attrs.get("end_time")
+        request = self.context.get("request")
 
-        appointment_date = attrs.get("appointment_date")
+    if start_time < timezone.now():
+        raise serializers.ValidationError("Appointment cannot be booked in the past.")
 
-        if doctor.role != doctor.Roles.DOCTOR:
-            raise serializers.ValidationError({
-                "doctor": "Selected user is not a doctor."
-            })
-
-        queryset = Appointment.objects.filter(doctor=doctor, appointment_date=appointment_date)
-
-        if self.instance:
-            queryset = queryset.exclude(pk=self.instance.pk)
-
-        if queryset.exists():
-            raise serializers.ValidationError({
-                "appointment_date": "Doctor already has an appointment at this time."
-            })
-
-        return attrs
+    if end_time <= start_time:
+        raise serializers.ValidationError("end_time must be after start_time.")
