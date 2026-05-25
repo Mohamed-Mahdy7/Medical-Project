1486a3ffa0c16d380a92c290c52edf24e1d7e2bd
mohammednasef97-stack <mohammed.nasef97@gmail.com>
2026-05-26 00:50:12 +0300

    gitignor modifiy

--- doctors/urls.py
+++ doctors/urls.py
@@ -4,7 +4,6 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import viewsets, permissions
-
 from .models import Specialty, DoctorProfile
 from .serializers import (
     SpecialtySerializer,
