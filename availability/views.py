447fd1d524d606a662a7a19c29ce528d668719a1
ZahwaKandeel <zahwa.bm.kandeel@gmail.com>
2026-05-25 22:51:43 +0300

    availability views with permission to doctor only

--- availability/views.py
+++ availability/views.py
@@ -1,12 +1,26 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from .models import Availability
 from .serializers import AvailabilitySerializer
+from appointments.permissions import IsDoctor
 
-from rest_framework import viewsets
-from rest_framework.response import Response
 
-# Create your views here.
-class AvailabilityViewSet(viewsets.ViewSet):
-    def list(self, request):
-        availabilities = Availability.objects.all()
-        serializer = AvailabilitySerializer(availabilities, many=True)
-        return Response(serializer.data)
+class AvailabilityViewSet(ModelViewSet):
+    queryset = Availability.objects.all()
+    serializer_class = AvailabilitySerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsAuthenticated(), IsDoctor()]
+        return [IsAuthenticated()]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.role == "D":
+            return Availability.objects.filter(doctor__user=user)
+
+        return Availability.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(doctor=self.request.user.doctorprofile)
