9d65dac0ae70c409530b2ad75193738c46f8e351
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2026-05-26 00:03:09 +0300

    implementd User ViewSet

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from . import views
 
-router = DefaultRouter
+router = DefaultRouter()
 router.register('user', views.UserViewSet)
 
+urlpatterns = router.urls
--- medical_project/urls.py
+++ medical_project/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('user/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     path('patients/', include('patients.urls')),
     path('availability/', include('availability.urls')),
     path("appointments/", include("appointments.urls")),
