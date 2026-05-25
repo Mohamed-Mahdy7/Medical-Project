6bc0f8a7dc13d8e38c699b793116c50cb95453f5
Fiow00 <ahmedyhya5325500@gmail.com>
2026-05-25 16:03:55 +0300

    Updated admin panel supporting user friendly interface

--- appointments/admin.py
+++ appointments/admin.py
@@ -2,4 +2,30 @@ from django.contrib import admin
 
 from .models import Appointment
 
-admin.site.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = [
+        'patient', 
+        'doctor', 
+        'status', 
+        'start_time', 
+        'end_time', 
+        'created_at'
+    ]
+
+    list_filter = [
+        'status', 
+        'doctor__specialty'
+    ]
+
+    search_fields = [
+        'patient__user__username', 
+        'patient__user__email',
+        'doctor__user__username', 
+        'doctor__user__email'
+    ]
+
+    ordering = ['start_time']
+
+    readonly_fields = ['id', 'created_at', 'updated_at']
+
+admin.site.register(Appointment, AppointmentAdmin)
