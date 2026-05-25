6f347f1dfa820935a14d03568f1ee9e6510c6314
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2026-05-26 01:18:33 +0300

    impleminted session auth

--- medical_project/urls.py
+++ medical_project/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('auth/session/', include('rest_framework.urls')),
     path('accounts/', include('accounts.urls')),
     path('patients/', include('patients.urls')),
     path('availability/', include('availability.urls')),
