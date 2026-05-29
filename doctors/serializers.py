f7816e95eba6190bbc41cb078862fd9eab13d652
YamenAly <yamen.halim@gmail.com>
2026-05-30 01:51:18 +0300

    slot generation done

--- doctors/serializers.py
+++ doctors/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
+from datetime import date
 from .models import (
     Specialty, DoctorProfile
 )
+from .utils import generate_available_slots
 
 
 class SpecialtySerializer(serializers.ModelSerializer):
@@ -15,6 +17,7 @@ class SpecialtySerializer(serializers.ModelSerializer):
 
 
 class DoctorProfileSerializer(serializers.ModelSerializer):
+    available_slots = serializers.SerializerMethodField()
     class Meta:
         model = DoctorProfile
         fields = [
@@ -24,5 +27,21 @@ class DoctorProfileSerializer(serializers.ModelSerializer):
             'bio',
             'phone',
             'profile_picture',
-            'years_of_experience'
+            'years_of_experience',
+            'available_slots'
         ]
+
+        def get_available_slots(self, obj):
+            request = self.context.get('request')
+            date_str = request.query_params.get('date') if request else None
+    
+            if date_str:
+                from datetime import datetime
+                try:
+                    query_date = datetime.strptime(date_str, '%Y-%m-%d').date()
+                except ValueError:
+                    return []
+            else:
+                query_date = date.today()
+    
+            return generate_available_slots(doctor_id=obj.pk, date=query_date)
