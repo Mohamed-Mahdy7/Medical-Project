75c867e00a8ef8d79625bf088e2497a3fd6a3b86
mohammednasef97-stack <mohammed.nasef97@gmail.com>
2026-05-30 07:40:34 +0300

    fixing admin doctor

--- doctors/admin.py
+++ doctors/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 
 from .models import Specialty, DoctorProfile
 
-admin.site.register(Specialty)
-admin.site.register(DoctorProfile)
+ 
 from django.contrib import admin
 from .models import DoctorProfile, Specialty
 
