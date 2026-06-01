b7ddb4c28bda1365b3d63059c82a68d58eb5a754
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2026-06-01 22:28:31 +0300

    implementing Doctor context

---
+++ Frontend/medical_project/src/context/DoctorContext.jsx
@@ -0,0 +1,39 @@
+ import { createContext, useEffect, useState } from "react";
+import { getDoctor,createSpecialty } from "../services/doctorservice.js";
+
+export const DoctorContext = createContext();
+
+export function DoctorProvider({ children }) {
+    const [doctor, setDoctor] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchDoctor = async () => {
+            try {
+                const res = await getDoctor();
+                setDoctor(res.data);
+            } catch (err) {
+                console.error("Failed to load doctor data", err);
+                setDoctor(null);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchDoctor();
+    }, []);
+    async function addspeciality(data) {
+        try { await createSpecialty(data); return true; } 
+        
+        catch (err) {
+            console.error("Failed to create specialty", err);
+            return false;
+        }
+    }
+
+    return (
+        <DoctorContext.Provider value={{ doctor, loading, addspeciality }}>
+            {children}
+        </DoctorContext.Provider>
+    );
+}
